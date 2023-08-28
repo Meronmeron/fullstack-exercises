9d7d613bdcc0b34c0f337f05ac554364eaed0825
Meronmeron <mmer3725@gmail.com>
2023-08-28 18:29:11 +0300

    Exercise Part1.b 1.3.-1.5 done

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -1,17 +1,27 @@
 import Part from "./Part"
 const Content =()=>{
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7
+          },
+          {
+            name: 'State of a component',
+            exercises: 14
+          }
+        ]
+      }
     return(
         <div>
-        <Part part1={part1} exercises1={exercises1} />
-        <Part part2={part2} exercises2={exercises2} />
-        <Part part3={part3} exercises3={exercises3} />
+        <Part part1={course.parts[0].name} exercises1={course.parts[0].exercises} />
+        <Part part2={course.parts[1].name} exercises2={course.parts[1].exercises} />
+        <Part part3={course.parts[2].name} exercises3={course.parts[2].exercises} />
 
         </div>
     )
